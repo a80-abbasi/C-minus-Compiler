eb30611c424a1e662dce1f5817b5603acdc9ec0f
tandalalam <maziar56bnd@gmail.com>
2021-10-23 10:19:26 +0330

    More test

--- Test.py
+++ Test.py
@@ -1,23 +1,40 @@
 import unittest
 from shutil import copyfile
 
+
 class Test(unittest.TestCase):
 
     @staticmethod
     def output(path):
+        out_string = ''
         with open(path, 'r') as f:
-            out_string = f.read()
+            while True:
+                line = f.readline()
+                if not line:
+                    break
+                out_string += line.strip()
         return out_string.strip('\n')
 
     def test_sample_1(self):
         for i in range(1, 11):
-            copyfile(f'PA1_testcases1.3/T{i:02d}/input.txt', 'input.txt')
-            with open('compiler.py') as compiler:
-                exec(compiler.read())
-            self.assertEqual(Test.output(f'PA1_testcases1.3/T{i:02d}/symbol_table.txt'), Test.output('symbol_table.txt'))
-            self.assertEqual(Test.output(f'PA1_testcases1.3/T{i:02d}/tokens.txt'), Test.output('tokens.txt'))
-            self.assertEqual(Test.output(f'PA1_testcases1.3/T{i:02d}/lexical_errors.txt'), Test.output('lexical_errors.txt'))
-            print(f'Test {i} Passed!')
+            with self.subTest(i=i):
+                copyfile(f'PA1_testcases1.3/T{i:02d}/input.txt', 'input.txt')
+                with open('compiler.py') as compiler:
+                    exec(compiler.read())
+                self.assertMultiLineEqual(Test.output(f'PA1_testcases1.3/T{i:02d}/symbol_table.txt'), Test.output('symbol_table.txt'))
+                self.assertMultiLineEqual(Test.output(f'PA1_testcases1.3/T{i:02d}/tokens.txt'), Test.output('tokens.txt'))
+                self.assertMultiLineEqual(Test.output(f'PA1_testcases1.3/T{i:02d}/lexical_errors.txt'), Test.output('lexical_errors.txt'))
+
+    def test_sample_2(self):
+        for i in range(11, 16):
+            with self.subTest(i=i):
+                copyfile(f'PA1_testcases1.3/T{i:02d}/input.txt', 'input.txt')
+                with open('compiler.py') as compiler:
+                    exec(compiler.read())
+                self.assertMultiLineEqual(Test.output(f'PA1_testcases1.3/T{i:02d}/tokens.txt'), Test.output('tokens.txt'))
+                self.assertMultiLineEqual(Test.output(f'PA1_testcases1.3/T{i:02d}/lexical_errors.txt'), Test.output('lexical_errors.txt'))
+                self.assertMultiLineEqual(Test.output(f'PA1_testcases1.3/T{i:02d}/symbol_table.txt'), Test.output('symbol_table.txt'))
+
 
 
 
