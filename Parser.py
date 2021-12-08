edc8c810437a97ca19aa95b30349f0f8198931d3
tandalalam <maziar56bnd@gmail.com>
2021-12-08 20:34:37 +0330

    create transition diagram completed

--- Parser.py
+++ Parser.py
@@ -21,12 +21,14 @@ def terminal_matches(la, t):  # todo
 
 
 class TransitionDiagram:
+
     grammar_address = 'c-minus_001'
 
     def __init__(self, parser):
-        self.start_symbols = self.create_transition_diagram()
         grammar_file = open(file=TransitionDiagram.grammar_address, mode='r')
+        self.start_symbols = self.create_transition_diagram()
         self.non_terminals = self.create_non_terminals()
+        self.terminals = []
         self.grammar = grammar_file.read().splitlines()
         self.state = self.start_symbols[0]
         self.parser = parser
@@ -37,9 +39,64 @@ class TransitionDiagram:
         state_number = 0
         for non_terminal, rule in enumerate(self.grammar):
             start_state = StartState(state_number, self.non_terminals[non_terminal])
-            start_symbols[non_terminal] = start_state
+            state_number += 1
 
-        return None
+            final_state = State(state_number, self.non_terminals[non_terminal], True)
+            state_number += 1
+
+            start_symbols[non_terminal] = start_state  # Add start state to output
+            _, right_hand_sides = rule.split('->')
+
+            for rhs in right_hand_sides.split('|'):
+                first_ntt_name, *other_ntt_name = rhs.split()
+                ntt = self.find_ntt(first_ntt_name)
+
+                depth_two_state = State(number=state_number, start_non_terminal=self.non_terminals[non_terminal])
+                start_state.add_neighbor(depth_two_state, ntt)  # first states after start state
+
+                state_number += 1
+
+                temporary_state = depth_two_state  # to make path neighbors
+
+                for other_name in other_ntt_name[0:-1]:
+                    next_ntt = self.find_ntt(other_name)
+                    next_state = State(state_number, self.non_terminals[non_terminal])
+
+                    state_number += 1
+
+                    temporary_state.add_neighbor(next_state, next_ntt)
+                    temporary_state = next_state
+
+                # add final state to last path state neighborhood
+                temporary_state.add_neighbor(final_state, self.find_ntt(other_ntt_name[-1]))
+
+        return start_symbols
+
+    def is_non_terminal(self, name):
+        for non_terminal in self.non_terminals:
+            if non_terminal.name == name:
+                return True, non_terminal
+        return False, None
+
+    def is_terminal(self, name):
+        for terminal in self.terminals:
+            if terminal.name == name:
+                return True, terminal
+        return False, None
+
+    def find_ntt(self, name):
+        is_non_terminal, non_terminal = self.is_non_terminal(name)
+        if is_non_terminal:
+            return non_terminal
+
+        is_terminal, terminal = self.is_terminal(name)
+        if is_terminal:
+            return terminal
+
+        terminal = NTT(number=-1, is_terminal=True, name=name)
+        self.terminals.append(terminal)
+
+        return terminal
 
     def create_non_terminals(self):
         non_terminals = []
@@ -80,10 +137,11 @@ class TransitionDiagram:
 
 
 class State:
-    def __init__(self, number, is_final=False, neighbors=None):
+    def __init__(self, number, start_non_terminal, is_final=False, neighbors=None):
         self.number = number
         self.is_final = is_final
         self.neighbors = neighbors
+        self.start_non_terminal = start_non_terminal
 
     def add_neighbor(self, neighbor, input):  # input is an NTT
         self.neighbors = input, neighbor
@@ -91,9 +149,8 @@ class State:
 
 class StartState(State):
 
-    def __init__(self, number, start_terminal, is_final=False, neighbors=None):
-        super().__init__(number, is_final, neighbors)
-        self.start_terminal = start_terminal
+    def __init__(self, number, start_non_terminal, is_final=False, neighbors=None):
+        super().__init__(number, start_non_terminal, is_final, neighbors)
         self.neighbors = neighbors if neighbors is not None else []
 
     def add_neighbor(self, neighbor, input):
@@ -115,7 +172,7 @@ class StartState(State):
                 ntt, cur = cur.neighbors
             # follow
             if 'epsilon' in condition:
-                condition.extend(self.start_terminal.follow)
+                condition.extend(self.start_non_terminal.follow)
             new_neighbors.append((input, neighbor, condition))
         self.neighbors = new_neighbors
 
